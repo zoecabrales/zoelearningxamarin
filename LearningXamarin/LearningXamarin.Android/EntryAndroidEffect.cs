ba8a649e87e328625f1d5bbc2d47d1a0e9c9cbc1
Zoe Cabrales <jirehoe@gmail.com>
2020-08-07 18:16:11 +0800

    behaviorsandentryeffect

--- LearningXamarin/LearningXamarin.Android/EntryAndroidEffect.cs
+++ LearningXamarin/LearningXamarin.Android/EntryAndroidEffect.cs
@@ -5,21 +5,21 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 [assembly: ResolutionGroupName("MyCompany")]
 [assembly: ExportEffect(typeof(XamarinStudy.Droid.Effects.EntryAndroidEffect), nameof(LearningXamarin.Effects.EntryEffect))]
 
-    namespace XamarinStudy.Droid.Effects
+namespace XamarinStudy.Droid.Effects
 {
     public class EntryAndroidEffect : PlatformEffect
     {
-        public object Control { get; private set; }
-
         protected override void OnAttached()
         {
             if (this.Control != null)
@@ -30,18 +30,7 @@ using Xamarin.Forms;
 
         protected override void OnDetached()
         {
-            throw new NotImplementedException();
-        }
-    }
-
-    internal class ColorDrawable
-    {
-        private Android.Graphics.Color transparent;
-
-        public ColorDrawable(Android.Graphics.Color transparent)
-        {
-            this.transparent = transparent;
+           
         }
     }
 }
-    
